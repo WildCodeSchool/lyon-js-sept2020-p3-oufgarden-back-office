df9506e8519da888263073450647d7aa821bcf14
AntoineGGG <a.gorenflot@gmx.fr>
2020-12-03 16:01:43 +0100

    navbar compoentn and scss and route

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './Navbar';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/adherents' />
-          <Route path='/adherents/:id' />
-          <Route path='/adherents/creation' />
-          <Route exact path='/articles' />
-          <Route exact path='/articles/:id' />
-          <Route path='/articles/creation' />
-          <Route exact path='/garden' />
-          <Route path='/garden/:id' />
-          <Route path='/garden/creation' />
-          <Route path='/calendar' />
+          <Route exact path="/adherents" />
+          <Route path="/adherents/:id" />
+          <Route path="/adherents/creation" />
+          <Route exact path="/articles" />
+          <Route exact path="/articles/:id" />
+          <Route path="/articles/creation" />
+          <Route exact path="/garden" />
+          <Route path="/garden/:id" />
+          <Route path="/garden/creation" />
+          <Route path="/calendar" />
         </Switch>
       </div>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './style/Navbar.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style/Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='logo'></div>
+    <div className="navbar">
+      <div className="logo"></div>
       <ul>
         <li>
-          <Link to='/adherents'>Adhérents</Link>
+          <Link to="/adherents">Adhérents</Link>
         </li>
         <li>
-          <Link to='/garden'>Jardins</Link>
+          <Link to="/garden">Jardins</Link>
         </li>
         <li>
-          <Link to='/articles'>Articles</Link>
+          <Link to="/articles">Articles</Link>
         </li>
         <li>
-          <Link to='/category'>Catégories</Link>
+          <Link to="/category">Catégories</Link>
         </li>
         <li>
-          <Link to='/calendar'>Agenda</Link>
+          <Link to="/calendar">Agenda</Link>
         </li>
       </ul>
     </div>
