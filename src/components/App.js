a91dd924203818e08ab14565c87525d3f0d7edfe
Pella0 <pellattiero.ju@gmail.com>
2020-12-09 11:12:27 +0100

    ajout login et css centre page et sa route vers composant

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import LoginF from './LoginF';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import LoginF from "./LoginF";
 
 const App = () => {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/admin' component={LoginF} />
-          <Route path='/admin/home' />
-          <Route exact path='/admin/adherents' />
-          <Route path='/admin/adherents/:id' />
-          <Route path='/admin/adherents/creation' />
-          <Route exact path='/admin/articles' />
-          <Route exact path='/admin/articles/:id' />
-          <Route path='/admin/articles/creation' />
-          <Route exact path='/admin/garden' />
-          <Route path='/admin/garden/:id' />
-          <Route path='/admin/garden/creation' />
-          <Route path='/admin/calendar' />
+          <Route exact path="/admin" component={LoginF} />
+          <Route path="/admin/home" />
+          <Route exact path="/admin/adherents" />
+          <Route path="/admin/adherents/:id" />
+          <Route path="/admin/adherents/creation" />
+          <Route exact path="/admin/articles" />
+          <Route exact path="/admin/articles/:id" />
+          <Route path="/admin/articles/creation" />
+          <Route exact path="/admin/garden" />
+          <Route path="/admin/garden/:id" />
+          <Route path="/admin/garden/creation" />
+          <Route path="/admin/calendar" />
         </Switch>
       </div>
     </Router>
