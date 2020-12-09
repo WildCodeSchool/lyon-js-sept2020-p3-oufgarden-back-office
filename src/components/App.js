7c042ba9e2230905131fe9ce0bb860bddf077c17
EddyM <eddy.mandran@gmail.com>
2020-12-09 10:34:52 +0100

    update route in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,18 @@ const App = () => {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path="/adherents" />
-          <Route exact path="/adherents/creation" component={MemberCreation} />
-          <Route exact path="/adherents/:id" />
-          <Route exact path="/articles" />
-          <Route exact path="/articles/:id" />
-          <Route path="/articles/creation" />
-          <Route exact path="/garden" />
-          <Route path="/garden/:id" />
-          <Route path="/garden/creation" />
-          <Route path="/calendar" />
+          <Route exact path="/admin" />
+          <Route path="/admin/home" />
+          <Route exact path="/admin/adherents" />
+          <Route path="/admin/adherents/:id" />
+          <Route path="/admin/adherents/creation" component={MemberCreation}/>
+          <Route exact path="/admin/articles" />
+          <Route exact path="/admin/articles/:id" />
+          <Route path="/admin/articles/creation" />
+          <Route exact path="/admin/garden" />
+          <Route path="/admin/garden/:id" />
+          <Route path="/admin/garden/creation" />
+          <Route path="/admin/calendar" />
         </Switch>
       </div>
     </Router>
