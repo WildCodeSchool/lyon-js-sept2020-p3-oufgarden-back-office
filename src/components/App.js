d6e6c2c7f803133a1ca7c14f6195d7ef10097e99
AntoineGGG <a.gorenflot@gmx.fr>
2020-12-09 10:21:02 +0100

    Add admin route

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './Navbar';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/admin' />
-          <Route path='/admin/home' />
-          <Route exact path='/admin/adherents' />
-          <Route path='/admin/adherents/:id' />
-          <Route path='/admin/adherents/creation' />
-          <Route exact path='/admin/articles' />
-          <Route exact path='/admin/articles/:id' />
-          <Route path='/admin/articles/creation' />
-          <Route exact path='/admin/garden' />
-          <Route path='/admin/garden/:id' />
-          <Route path='/admin/garden/creation' />
-          <Route path='/admin/calendar' />
+          <Route exact path="/admin" />
+          <Route path="/admin/home" />
+          <Route exact path="/admin/adherents" />
+          <Route path="/admin/adherents/:id" />
+          <Route path="/admin/adherents/creation" />
+          <Route exact path="/admin/articles" />
+          <Route exact path="/admin/articles/:id" />
+          <Route path="/admin/articles/creation" />
+          <Route exact path="/admin/garden" />
+          <Route path="/admin/garden/:id" />
+          <Route path="/admin/garden/creation" />
+          <Route path="/admin/calendar" />
         </Switch>
       </div>
     </Router>
