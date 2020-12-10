a22703c069de92d0709cf1e654809819767624bf
AntoineGGG <a.gorenflot@gmx.fr>
2020-12-10 13:26:56 +0100

    Login ok, redirect to adhrents page

--- src/components/LoginF.js
+++ src/components/LoginF.js
@@ -1,70 +1,74 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import "./style/Login.scss";
-import axios from "axios";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './style/Login.scss';
+import axios from 'axios';
 
-require("dotenv").config();
+require('dotenv').config();
 
-const required = "Ce champs est requis";
+const required = 'Ce champs est requis';
 
 const errorMessage = (error) => {
-  return <div className="invalid-feedback">{error}</div>;
+  return <div className='invalid-feedback'>{error}</div>;
 };
 
-const LoginF = () => {
+const LoginF = (props) => {
   const { register, handleSubmit, errors } = useForm();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
 
   const url = process.env.REACT_APP_API_BASE_URL;
 
   const onSubmit = (data) => {
-    console.log(data);
-    axios.post(`${url}/login`).then((res) => {
-      res
-        .status(200)
-        .send("loged")
-        .catch((err) => {
-          console.log("error log");
-        });
-    });
+    axios
+      .post(`${url}/login`, data)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data === 'logged') {
+          props.history.push('/adherents');
+          setIsLogged(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
-    <div className="container">
-      <div className="box">
-        <h3>Se connecter</h3>
+    <div className='container'>
+      <div className='box'>
+        <h3>Log in</h3>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="input-wrapper">
-            <div className="field">
+          <div className='input-wrapper'>
+            <div className='field'>
               <input
                 onChange={(event) => setEmail(event.target.value)}
                 value={email}
-                className="form-control"
-                type="email"
-                placeholder="Email"
-                name="Email"
+                className='form-control'
+                type='email'
+                placeholder='Email'
+                name='email'
                 ref={register({ required: true, pattern: /^\S+@\S+$/i })}
               />
               {errors.Email &&
-                errors.Email.type === "required" &&
+                errors.Email.type === 'required' &&
                 errorMessage(required)}
 
               <input
                 onChange={(event) => setPassword(event.target.value)}
                 value={password}
-                className="form-control"
-                type="password"
-                placeholder="Password"
-                name="Password"
+                className='form-control'
+                type='password'
+                placeholder='Password'
+                name='password'
                 ref={register({ required: true })}
               />
               {errors.Password &&
-                errors.Password.type === "required" &&
+                errors.Password.type === 'required' &&
                 errorMessage(required)}
 
-              <div className="form-group">
-                <button type="submit" className="button">
+              <div className='form-group'>
+                <button type='submit' className='button'>
                   connexion
                 </button>
               </div>
