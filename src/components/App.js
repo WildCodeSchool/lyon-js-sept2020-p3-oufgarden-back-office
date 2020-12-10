9561899a6a94f930019838631266f43aaa7b6a7b
AntoineGGG <a.gorenflot@gmx.fr>
2020-12-10 13:27:03 +0100

    Login ok, redirect to adhrents page

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import LoginF from './LoginF';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import LoginF from "./LoginF";
 
 const App = () => {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/' component={LoginF} />
-          <Route path='/home' />
-          <Route exact path='/adherents' />
-          <Route path='/adherents/:id' />
-          <Route path='/adherents/creation' />
-          <Route exact path='/articles' />
-          <Route exact path='/articles/:id' />
-          <Route path='/articles/creation' />
-          <Route exact path='/garden' />
-          <Route path='/garden/:id' />
-          <Route path='/garden/creation' />
-          <Route path='/calendar' />
+          <Route exact path="/" component={LoginF} />
+          <Route path="/home" />
+          <Route exact path="/adherents" />
+          <Route path="/adherents/:id" />
+          <Route path="/adherents/creation" />
+          <Route exact path="/articles" />
+          <Route exact path="/articles/:id" />
+          <Route path="/articles/creation" />
+          <Route exact path="/garden" />
+          <Route path="/garden/:id" />
+          <Route path="/garden/creation" />
+          <Route path="/calendar" />
         </Switch>
       </div>
     </Router>
