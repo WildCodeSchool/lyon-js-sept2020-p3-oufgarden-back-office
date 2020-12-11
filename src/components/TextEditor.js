1a6164d2f775ccd86ab795bf380a8a153dce5e77
AlexButeau <alexbuteau1@gmail.com>
2020-12-11 10:50:48 +0100

    created function to create a new article

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 // import { useMutation } from "react-query";
-import { Editor } from '@tinymce/tinymce-react';
-import { makeEntityAdder } from '../services/API';
+import { Editor } from "@tinymce/tinymce-react";
+import { makeEntityAdder } from "../services/API";
 
 const TextEditor = () => {
-  const [articleContent, setArticleContent] = useState('');
-  const [title, setTitle] = useState('');
-  const [urlImage, setUrlImage] = useState('');
+  const [articleContent, setArticleContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [urlImage, setUrlImage] = useState("");
   let history = useHistory();
 
   const handleEditorChange = (content, editor) => {
@@ -16,8 +16,8 @@ const TextEditor = () => {
 
   const currentDate = new Date()
     .toISOString()
-    .replace('T', ' ')
-    .replace('Z', '');
+    .replace("T", " ")
+    .replace("Z", "");
 
   const handleTitle = (e) => {
     setTitle(e.target.value);
@@ -33,40 +33,40 @@ const TextEditor = () => {
       url: urlImage,
       created_at: currentDate,
     };
-    makeEntityAdder('articles')(data);
-    setArticleContent('');
-    setTitle('');
-    setUrlImage('');
-    history.push('/articles');
+    makeEntityAdder("articles")(data);
+    setArticleContent("");
+    setTitle("");
+    setUrlImage("");
+    history.push("/articles");
   };
 
   return (
     <div>
-      <input className='Title' placeholder='Titre' onChange={handleTitle} />
+      <input className="Title" placeholder="Titre" onChange={handleTitle} />
       <input
-        className='Image'
+        className="Image"
         placeholder="url de l'image"
         onChange={handleImage}
       />
       <Editor
-        apiKey='c2pe0rrg2r5r6yu3c3gcxe5qr192kgoghmxbjnaj4fk1yv9d'
-        initialValue=''
+        apiKey="c2pe0rrg2r5r6yu3c3gcxe5qr192kgoghmxbjnaj4fk1yv9d"
+        initialValue=""
         init={{
           height: 500,
           menubar: false,
           plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount',
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
           ],
           toolbar:
-            'undo redo | formatselect | bold italic backcolor | \
+            "undo redo | formatselect | bold italic backcolor | \
               alignleft aligncenter alignright alignjustify | \
-              bullist numlist outdent indent | removeformat | help',
+              bullist numlist outdent indent | removeformat | help",
         }}
         onEditorChange={handleEditorChange}
       />
-      <button className='sendButton' onClick={handleClick}>
+      <button className="sendButton" onClick={handleClick}>
         Créer
       </button>
     </div>
