6250844f636dd85fd8ccd6f655c2772df05ed18e
Nathan-Vanstaevel <nathanvanstaevel@gmail.com>
2020-12-11 14:38:00 +0100

    article and textedit components all right

--- src/components/ArticleCreation.js
+++ src/components/ArticleCreation.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import TextEditor from "./TextEditor";
 import "./style/TextEditor.scss";
-import "./style/Articles.scss";
+// import "./style/Articles.scss";
 
 const ArticleCreation = () => {
   return (
     <div>
-      <button className="buttonList">
-        <Link to="/articles">Listes</Link>
-      </button>
-      <button className="buttonArticle">
-        <Link to="/articles/creation">Nouvel Article</Link>
-      </button>
+      <div className="buttons">
+        <button className="buttonList">
+          <Link to="/articles">Listes</Link>
+        </button>
+        <button className="buttonArticle">
+          <Link to="/articles/creation">Nouvel Article</Link>
+        </button>
+      </div>
       <div className="TextEditor">
         <div className="Editor">
           <TextEditor />
--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-// import { useMutation } from "react-query";
 import { Editor } from "@tinymce/tinymce-react";
 import { makeEntityAdder } from "../services/API";
 
