a74c235e8e0e38a372c298ae33b57a240425b12b
AntoineGGG <a.gorenflot@gmx.fr>
2020-12-11 10:26:20 +0100

    Css to userlist

--- src/components/Adherents.js
+++ src/components/Adherents.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiMail } from 'react-icons/fi';
+import { MdDelete, MdEdit } from 'react-icons/md';
+import { IconContext } from 'react-icons';
+
 import { getCollection } from '../services/API';
 import './style/AdherentList.scss';
 
@@ -14,13 +19,31 @@ const Adherents = () => {
   }, []);
 
   return (
-    <div>
+    <div className="user-list-container">
+      <div className="buttonArticle">
+        <button type="button" className="button-user-list">
+          <Link to="/adherents">Listes</Link>
+        </button>
+        <button type="button" className="button-user">
+          <Link to="/adherents/creation">Nouvel Article</Link>
+        </button>
+      </div>
       {adherentList.map((e) => {
         return (
           <div key={e.id} className="adherent-row">
-            <p>
-              {e.firstname} {e.lastname} {e.email}
-            </p>
+            <div className="user-infos">
+              <p>
+                {e.firstname} {e.lastname} {e.email}
+              </p>
+            </div>
+            <div className="user-list-icons">
+              {/* IconContext provider pour personnaliser les props de react-icons */}
+              <IconContext.Provider value={{ className: 'react-icons' }}>
+                <FiMail size={25} style={{}} />
+                <MdEdit size={25} />
+                <MdDelete size={25} />
+              </IconContext.Provider>
+            </div>
           </div>
         );
       })}
