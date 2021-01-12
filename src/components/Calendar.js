74e717a4788fe0ac19eef349f40db52e3b5819c0
eddymandran <eddy.mandran@gmail.com>
2021-01-12 16:05:27 +0100

    fixed some stuff

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import './style/Calendar.scss';
 
@@ -11,9 +12,25 @@ function Calendar() {
   return (
     <div className="calendar">
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: 'dayGridMonth,timeGridWeek,timeGridDay',
+          center: 'title',
+          right: 'prevYear,prev,next,nextYear',
+        }}
+        views={{
+          dayGridMonth: {
+            // name of view
+            titleFormat: { day: '2-digit', month: '2-digit', year: 'numeric' },
+            // other view-specific options here
+          },
+        }}
         dateClick={handleDateClick}
-        initialView="dayGridMonth"
+        initialView="timeGridWeek"
+        events={[
+          { title: 'event 1', date: '2021-01-13' },
+          { title: 'event 2', date: '2021-01-15' },
+        ]}
       />
     </div>
   );
