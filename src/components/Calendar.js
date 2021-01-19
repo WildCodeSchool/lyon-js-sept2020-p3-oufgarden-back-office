0414f6e76cb8e96e235d5899da94c768ffa1e9ef
eddymandran <eddy.mandran@gmail.com>
2021-01-19 12:41:48 +0100

    implementation of the week / month view change

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,215 +1,18 @@
 import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
-import TableCell from '@material-ui/core/TableCell';
-import {
-  darken,
-  fade,
-  lighten,
-} from '@material-ui/core/styles/colorManipulator';
-import Typography from '@material-ui/core/Typography';
 import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
-import classNames from 'clsx';
 import {
   Scheduler,
-  MonthView,
+  DayView,
+  WeekView,
   Appointments,
   Toolbar,
-  DateNavigator,
-  AppointmentTooltip,
-  AppointmentForm,
-  EditRecurrenceMenu,
-  Resources,
-  DragDropProvider,
+  ViewSwitcher,
 } from '@devexpress/dx-react-scheduler-material-ui';
-import { withStyles } from '@material-ui/core/styles';
-import { users, appointments } from './CalendarData';
-
 import './style/Calendar.scss';
 
-const resources = [
-  {
-    fieldName: 'userId',
-    title: 'Participant',
-    instances: users,
-  },
-];
+import { appointments } from './CalendarData';
 
-const getBorder = (theme) =>
-  `1px solid ${
-    theme.palette.type === 'light'
-      ? lighten(fade(theme.palette.divider, 1), 0.88)
-      : darken(fade(theme.palette.divider, 1), 0.68)
-  }`;
-
-const DayScaleCell = (props) => (
-  <MonthView.DayScaleCell
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    {...props}
-    style={{ textAlign: 'center', fontWeight: 'bold' }}
-  />
-);
-
-const styles = (theme) => ({
-  cell: {
-    color: '#78909C!important',
-    position: 'relative',
-    userSelect: 'none',
-    verticalAlign: 'top',
-    padding: 0,
-    height: 100,
-    borderLeft: getBorder(theme),
-    '&:first-child': {
-      borderLeft: 'none',
-    },
-    '&:last-child': {
-      paddingRight: 0,
-    },
-    'tr:last-child &': {
-      borderBottom: 'none',
-    },
-    '&:hover': {
-      backgroundColor: 'white',
-    },
-    '&:focus': {
-      backgroundColor: fade(theme.palette.primary.main, 0.15),
-      outline: 0,
-    },
-  },
-  content: {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    alignItems: 'center',
-  },
-  text: {
-    padding: '0.5em',
-    textAlign: 'center',
-  },
-  opacity: {
-    opacity: '0.5',
-  },
-  appointment: {
-    borderRadius: '10px',
-    '&:hover': {
-      opacity: 0.6,
-    },
-  },
-  apptContent: {
-    '&>div>div': {
-      whiteSpace: 'normal !important',
-      lineHeight: 1.2,
-    },
-  },
-  flexibleSpace: {
-    flex: 'none',
-  },
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  tooltipContent: {
-    padding: theme.spacing(3, 1),
-    paddingTop: 0,
-    backgroundColor: theme.palette.background.paper,
-    boxSizing: 'border-box',
-    width: '400px',
-  },
-  tooltipText: {
-    ...theme.typography.body2,
-    display: 'inline-block',
-  },
-  title: {
-    ...theme.typography.h6,
-    color: theme.palette.text.secondary,
-    fontWeight: theme.typography.fontWeightBold,
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-  },
-  icon: {
-    color: theme.palette.action.active,
-    verticalAlign: 'middle',
-  },
-  circle: {
-    width: theme.spacing(4.5),
-    height: theme.spacing(4.5),
-    verticalAlign: 'super',
-  },
-  textCenter: {
-    textAlign: 'center',
-  },
-  dateAndTitle: {
-    lineHeight: 1.1,
-  },
-  titleContainer: {
-    paddingBottom: theme.spacing(2),
-  },
-  container: {
-    paddingBottom: theme.spacing(1.5),
-  },
-});
-
-// #FOLD_BLOCK
-const CellBase = React.memo(
-  ({
-    classes,
-    startDate,
-    formatDate,
-    otherMonth,
-    // #FOLD_BLOCK
-  }) => {
-    const isFirstMonthDay = startDate.getDate() === 1;
-    const formatOptions = isFirstMonthDay
-      ? { day: 'numeric', month: 'long' }
-      : { day: 'numeric' };
-    return (
-      <TableCell
-        tabIndex={0}
-        className={classNames({
-          [classes.cell]: true,
-          [classes.opacity]: otherMonth,
-        })}
-      >
-        <div className={classes.text}>
-          {formatDate(startDate, formatOptions)}
-        </div>
-      </TableCell>
-    );
-  }
-);
-const TimeTableCell = withStyles(styles, { name: 'Cell' })(CellBase);
-
-const Appointment = withStyles(styles, {
-  name: 'Appointment',
-})(({ classes, ...restProps }) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Appointments.Appointment {...restProps} className={classes.appointment} />
-));
-
-const AppointmentContent = withStyles(styles, {
-  name: 'AppointmentContent',
-})(({ classes, ...restProps }) => (
-  <Appointments.AppointmentContent
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    {...restProps}
-    className={classes.apptContent}
-  />
-));
-
-const FlexibleSpace = withStyles(styles, { name: 'ToolbarRoot' })(
-  ({ classes, ...restProps }) => (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <Toolbar.FlexibleSpace {...restProps} className={classes.flexibleSpace}>
-      <div className={classes.flexContainer}>
-        <Typography variant="h5" style={{ marginLeft: '10px' }}>
-          Garden calendar
-        </Typography>
-      </div>
-    </Toolbar.FlexibleSpace>
-  )
-);
 const Calendar = () => {
   const [data, setData] = useState(appointments);
 
@@ -240,28 +43,19 @@ const Calendar = () => {
   return (
     <div className="calendar">
       <Paper>
-        <Scheduler data={data}>
+        <Scheduler data={data} height={660}>
           <EditingState onCommitChanges={commitChanges} />
-          <ViewState defaultCurrentDate="2018-07-17" />
-
-          <MonthView
-            timeTableCellComponent={TimeTableCell}
-            dayScaleCellComponent={DayScaleCell}
+          <ViewState
+            defaultCurrentDate="2018-07-25"
+            defaultCurrentViewName="Week"
           />
 
-          <Appointments
-            appointmentComponent={Appointment}
-            appointmentContentComponent={AppointmentContent}
-          />
-          <Resources data={resources} />
+          <DayView startDayHour={9} endDayHour={18} />
+          <WeekView startDayHour={10} endDayHour={19} />
 
-          <Toolbar flexibleSpaceComponent={FlexibleSpace} />
-          <DateNavigator />
-
-          <EditRecurrenceMenu />
-          <AppointmentTooltip showCloseButton showDeleteButton showOpenButton />
-          <AppointmentForm />
-          <DragDropProvider />
+          <Toolbar />
+          <ViewSwitcher />
+          <Appointments />
         </Scheduler>
       </Paper>
     </div>
