5d361fc4d6e2afce7a76645c7efc32920a414cab
AlexButeau <alexbuteau1@gmail.com>
2021-01-28 12:18:44 +0100

    added iframe from oufgarden

--- src/components/PublicCalendar.js
+++ src/components/PublicCalendar.js
@@ -6,8 +6,8 @@ const PublicCalendar = () => {
     <div className="iframe-container">
       <iframe
         title="agenda-iframe"
-        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%237CB342&amp;ctz=Europe%2FParis&amp;src=dGVhbW91ZmdhcmRlbkBnbWFpbC5jb20&amp;src=ZnIuZnJlbmNoI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%230B8043"
-        style={{ border: 'solid 1px #777' }}
+        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FParis&amp;title=%C3%89v%C3%A8nements%20de%20Ouf&amp;showTitle=1&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;showTz=0&amp;mode=MONTH&amp;src=N29hZ2ZhZWM1MjdhdjdjcnIwN3ZsYWVnMzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%230B8043"
+        style={{ borderWidth: '0' }}
         width="800"
         height="600"
         frameBorder="0"
